528fdad26e1bcf5cc7a1f49831986eec309d1726
Margarita-999 <mrgnazarova@gmail.com>
2025-03-23 18:34:07 +0200

    Update hw03.rs

---
+++ src/homework/hw03.rs
@@ -0,0 +1,16 @@
+const W: u32 = 30;
+const H: u32 = 15;
+
+fn main() {
+    for y in 0..H {
+        for x in 0..W {
+            let border: bool = y == 0 || y == H - 1 || x == 0 || x == W - 1;
+            let diagonal: bool = x == 2 * y;
+            let diagonal2: bool = x == W - 1 - 2 * y;
+            let show: bool =  border || diagonal || diagonal2;
+            let sym: &str =  if show { "*" } else { " "};
+            print!("{}", sym);
+        }
+        println!();
+    } 
+}    

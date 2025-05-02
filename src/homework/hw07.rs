74b27d8f8932f4a47764d9c040a7d3dbf0f88c3a
Margarita-999 <mrgnazarova@gmail.com>
2025-05-02 15:08:34 +0300

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,34 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_lowercase() {
+                c.to_uppercase().to_string()
+            } else if c.is_uppercase() {
+                c.to_lowercase().to_string()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test_invert_the_case() {
+    let data =
+        [
+            ("Hello", "hELLO"),
+            ("World", "wORLD"),
+        ];
+    data
+        .iter()
+        .for_each(|(a, b)| {
+            assert_eq!(
+                invert_the_case(a.to_string()),
+                b.to_string()
+            );
+            assert_eq!(
+                invert_the_case(b.to_string()),
+                a.to_string()
+            );
+        });
+}
